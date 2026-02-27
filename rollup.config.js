6184c8f06f9f9e6a03231354fd2a9aa60d4373a5
mengxiangxin <mengxiangxin@ttpark.cn>
2026-02-27 14:53:31 +0800

    [A]增加less文件编译，解决react版本不同问题，增加打包修改热更新功能

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import alias from '@rollup/plugin-alias';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import path from 'path';
 
 export default {
   input: 'src/index.js',
@@ -19,11 +22,33 @@ export default {
       sourcemap: true
     }
   ],
+  watch: {
+    buildDelay: 300,
+    clearScreen: false
+  },
   plugins: [
-    resolve(),
+    resolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      mainFields: ['module', 'main'],
+      preferBuiltins: false
+    }),
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, 'src')
+        }
+      ]
+    }),
+    postcss({
+      extensions: ['.less', '.css'],
+      use: {
+        less: {},
+      },
+    }),
     babel({
       exclude: 'node_modules/**',
-      presets: ['@babel/preset-env', '@babel/preset-react'],
+      presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }]],
       babelHelpers: 'bundled'
     }),
     commonjs(),
